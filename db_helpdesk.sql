1f61bd5e818693b368a2da0e0fd689adb2d29b6a
baydhowi15 <baydhowi15@gmail.com>
2024-05-15 09:48:56 +0700

    tambah file database, edit

--- db_helpdesk.sql
+++ db_helpdesk.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Mar 26, 2024 at 06:45 AM
+-- Generation Time: May 15, 2024 at 04:47 AM
 -- Server version: 10.4.27-MariaDB
 -- PHP Version: 8.0.25
 
@@ -24,6 +24,34 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `detail_tiket`
+--
+
+CREATE TABLE `detail_tiket` (
+  `id_detail` int(11) NOT NULL,
+  `tiket_id` int(11) DEFAULT NULL,
+  `tanggapan` text NOT NULL,
+  `gambar_tanggapan` text NOT NULL,
+  `waktu_tanggapan` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `detail_tiket`
+--
+
+INSERT INTO `detail_tiket` (`id_detail`, `tiket_id`, `tanggapan`, `gambar_tanggapan`, `waktu_tanggapan`) VALUES
+(46, 46, 'Oke Siap saya otw', '20240428221114.png', '2024-04-28'),
+(47, 47, 'Siap saya segera', '20240428221156.png', '2024-04-28'),
+(48, 48, 'segera', 'default.jpg', '2024-05-02'),
+(49, 49, 'n', 'default.jpg', '2024-05-02'),
+(50, 50, 'oke', '20240502142717.png', '2024-05-02'),
+(51, 51, 's', 'default.jpg', '2024-05-02'),
+(52, 52, 'oke', 'default.jpg', '2024-05-02'),
+(53, 53, 'Segera ke tempat', 'default.jpg', '2024-05-15');
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `divisi`
 --
 
@@ -37,9 +65,17 @@ CREATE TABLE `divisi` (
 --
 
 INSERT INTO `divisi` (`id_divisi`, `divisi`) VALUES
-(1, 'Operasional Staff'),
-(3, 'IT'),
-(5, 'HR');
+(9, 'Komisaris'),
+(10, 'Direksi'),
+(11, 'Manager'),
+(12, 'Asistant Manager'),
+(13, 'Legal'),
+(14, 'Administrasi'),
+(15, 'Survey'),
+(16, 'SKPT'),
+(17, 'Keuangan'),
+(18, 'General Affair'),
+(19, 'IT');
 
 -- --------------------------------------------------------
 
@@ -57,8 +93,17 @@ CREATE TABLE `jabatan` (
 --
 
 INSERT INTO `jabatan` (`id_jabatan`, `jabatan`) VALUES
-(3, 'Supervisior'),
-(4, 'Assistant Toko');
+(19, 'Komisaris'),
+(20, 'Direksi'),
+(21, 'Manager'),
+(22, 'Assisant Manager'),
+(23, 'Admin Legal'),
+(24, 'Staff Admin'),
+(25, 'Staff Survey'),
+(26, 'Staff SKPT'),
+(27, 'Staff Keuangan'),
+(28, 'Staff General Affair'),
+(29, 'Staff IT');
 
 -- --------------------------------------------------------
 
@@ -68,7 +113,7 @@ INSERT INTO `jabatan` (`id_jabatan`, `jabatan`) VALUES
 
 CREATE TABLE `tiket` (
   `id_tiket` int(11) NOT NULL,
-  `no_tiket` text NOT NULL,
+  `no_tiket` varchar(25) NOT NULL,
   `judul_tiket` text NOT NULL,
   `deskripsi` text NOT NULL,
   `gambar_tiket` text DEFAULT NULL,
@@ -76,6 +121,14 @@ CREATE TABLE `tiket` (
   `status_tiket` int(2) NOT NULL,
   `tgl_daftar` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `tiket`
+--
+
+INSERT INTO `tiket` (`id_tiket`, `no_tiket`, `judul_tiket`, `deskripsi`, `gambar_tiket`, `user_id`, `status_tiket`, `tgl_daftar`) VALUES
+(53, '1505240001', 'Pc Blue screen', 'Pc Blue screen', '150524000120240515092922.png', 43, 2, '2024-05-15 02:30:10'),
+(54, '1505240002', 'Tolong install ulang', 'Tolong install ulang', 'default.jpg', 42, 0, '2024-05-14 17:00:00');
 
 -- --------------------------------------------------------
 
@@ -103,13 +156,20 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`id_users`, `nip`, `username`, `email`, `password`, `jabatan`, `divisi`, `created`, `modified`, `image_user`, `level_user`, `status_user`) VALUES
-(22, 123678, 'odi', 'odi@gmail.com', '$2y$10$.l6eKq2XRY8BlCjBg6BtJu.KXrsTlaBs/dijS1UkN2NYIIYXl3AX.', 'Supervisior', 'Operasional Staff', '2024-03-26 04:33:45', '2024-03-26 04:33:45', '', 1, 1),
-(23, 876868, 'beni', 'beni@gmail.com', '$2y$10$QgR/s0uaHtWx4d8Zt/boZezQdRL6.jvMMmeoIPWqSNWJHIfYOzJNe', 'Supervisior', 'Operasional Staff', '2024-03-26 04:40:35', '2024-03-26 05:31:47', '', 1, 1),
-(26, 98798, 'ad', 'ad@gmail.com', '$2y$10$AXYf7JStkD6ysWWSd2ViNuM.vGzBZCm22pVarKA./iHvNoLHL2vbm', '', '', '2024-03-26 05:16:36', '2024-03-26 05:16:36', '', 1, 1);
+(39, 98789, 'admin', 'admin@gmail.com', '$2y$10$tWf9mjvzEvc/99F0L.9IDulqVg2VUocubMRQB4LNp9TsnSTAQIVkq', 'Staff IT', 'IT', '2024-05-15 01:52:11', '2024-05-15 02:24:28', '', 2, 1),
+(41, 10001, 'suharjono', 'suharjo@gmail.com', '$2y$10$fSQu1DnsWQLs.Ra.8NHQb.RFrOPlUU9298TYhRcIyhbpAx2XY/sgG', 'Direksi', 'Direksi', '2024-05-15 02:27:13', '2024-05-15 02:27:13', '', 1, 1),
+(42, 1005, 'fachren', 'fachren@gmail.com', '$2y$10$n8ao9doSfbK44duh/t4uY.yKYMTz23MYGcqsZR33sDmkxeIi6rvt2', 'Assisant Manager', 'Asistant Manager', '2024-05-15 02:27:47', '2024-05-15 02:27:47', '', 1, 1),
+(43, 10008, 'agam', 'agam@gmail.com', '$2y$10$HW4oH5WknkW8q8i2RrS/8eeWKJhxUW5kFPrr2Sx/0uL0cmcvw9DuS', 'Staff Admin', 'Administrasi', '2024-05-15 02:28:24', '2024-05-15 02:28:24', '', 1, 1);
 
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `detail_tiket`
+--
+ALTER TABLE `detail_tiket`
+  ADD PRIMARY KEY (`id_detail`);
 
 --
 -- Indexes for table `divisi`
@@ -141,28 +201,34 @@ ALTER TABLE `users`
 --
 
 --
+-- AUTO_INCREMENT for table `detail_tiket`
+--
+ALTER TABLE `detail_tiket`
+  MODIFY `id_detail` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=54;
+
+--
 -- AUTO_INCREMENT for table `divisi`
 --
 ALTER TABLE `divisi`
-  MODIFY `id_divisi` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `id_divisi` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=20;
 
 --
 -- AUTO_INCREMENT for table `jabatan`
 --
 ALTER TABLE `jabatan`
-  MODIFY `id_jabatan` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+  MODIFY `id_jabatan` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=30;
 
 --
 -- AUTO_INCREMENT for table `tiket`
 --
 ALTER TABLE `tiket`
-  MODIFY `id_tiket` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id_tiket` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=55;
 
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id_users` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=27;
+  MODIFY `id_users` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=44;
 
 --
 -- Constraints for dumped tables
